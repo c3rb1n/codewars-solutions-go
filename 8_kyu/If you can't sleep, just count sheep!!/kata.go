948cf4b737580bb929ecb2e2bcff5ad68b2e729a
c3rb1n <ilya.prudnikov.97@gmail.com>
2025-10-31 00:19:22 +0300

    added solution for "If you can't sleep, just count sheep!!" kata (8 kyu)

---
+++ 8_kyu/If you can't sleep, just count sheep!!/kata.go
@@ -0,0 +1,13 @@
+package kata
+
+import "fmt"
+
+func countSheep(num int) string {
+	var result string
+
+	for i := 1; i <= num; i++ {
+		result += fmt.Sprintf("%d sheep...", i)
+	}
+
+	return result
+}
